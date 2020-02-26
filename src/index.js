f90d95da87e212de45a90d9a410e890e21c64185
John(Jack) Jamison <37374979+jackjohnjamison@users.noreply.github.com>
2020-02-26 19:06:04 +0000

    Drivable car
    You can drive a car arround on massive grass!

--- src/index.js
+++ src/index.js
@@ -3,59 +3,82 @@ require('three/examples/js/loaders/GLTFLoader')
 
 
 var scene = new THREE.Scene()
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight)
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 4000)
 
 var renderer = new THREE.WebGLRenderer({antialias: true})
+const texLoader = new THREE.TextureLoader();
+
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setClearColor( 0xcccccc )
 document.body.append(renderer.domElement)
 
-// var geometry = new THREE.BoxGeometry(1,1,1)
-// var material = new THREE.MeshPhongMaterial({color: 0xCCCCCC})
-// var cube = new THREE.Mesh(geometry,material)
-// scene.add(cube)
+///////////////////////////////////////////////////////
+
+var floorGeometry = new THREE.BoxGeometry(4000, 1, 4000)
+var floorMaterial = new THREE.MeshPhongMaterial( { map: texLoader.load('dist/images/grass.jpg'), shininess: 30 } );
+var floor = new THREE.Mesh( floorGeometry, floorMaterial )
+scene.add(floor)
+
+///////////////////////////////////////////////////////
 
 var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 )
 scene.add( light )
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 )
+var directionalLight = new THREE.DirectionalLight( 0x000000, 1.5 )
 scene.add( directionalLight )
 
 
 ///////////////////////////////////////////////////////
+
+var myCar
 
 var loader = new THREE.GLTFLoader()
 
 loader.load( 'dist/models/car/scene.gltf', function(gltf){
     scene.add(gltf.scene)
     myCar = gltf.scene
-    console.log(myCar)
     addControles(myCar)
     setPosition(myCar)
+    animate()
 }, undefined, function(error){
 	console.error( error )
 })
 
 ///////////////////////////////////////////////////////
+var arrowFoward = false
+var arrowBack = false
+var arrowRight = false
+var arrowLeft = false
 
 function addControles(object) {
-    acceleration = 10
-
-
     document.addEventListener('keydown', event => {
         event.preventDefault()
         if(event.keyCode === 38) {
-            object.position.z  += acceleration * Math.cos(object.rotation.y)
-            object.position.x += acceleration * Math.sin(object.rotation.y)
+            arrowFoward = true
         }
         if(event.keyCode === 40) {
-            object.position.z -= 10
+            arrowBack = true
         }
         if(event.keyCode === 37) {
-            object.rotation.y += 0.2
+            arrowRight = true
         }
         if(event.keyCode === 39) {
-            object.rotation.y -= 0.2
+            arrowLeft = true
+        }
+    })
+
+    document.addEventListener("keyup", event => {
+        if (event.keyCode === 38) {
+            arrowFoward = false
+        }
+        if (event.keyCode === 40) {
+            arrowBack = false
+        }
+        if(event.keyCode === 37) {
+            arrowRight = false
+        }
+        if(event.keyCode === 39) {
+            arrowLeft = false
         }
     })
 }
@@ -66,16 +89,36 @@ function setPosition(object) {
     object.scale.x = 0.2
     object.scale.y = 0.2
     object.scale.z = 0.2
+
+    floor.position.z = -1000
+    floor.position.y = -200
 }
 
 ///////////////////////////////////////////////////////
 
-// cube.position.z = -5
+var acceleration = 2
+var friction = 0.9
+var turnSpeed = 0.1
+var xVelocity = 0
+var zVelocity = 0
+
 var animate = function() {
 
-    // cube.rotation.x += 0.01
+    zVelocity += (acceleration * Math.cos(myCar.rotation.y)) * arrowFoward
+    zVelocity -= (acceleration * Math.cos(myCar.rotation.y)) * arrowBack
+    
+    xVelocity += (acceleration * Math.sin(myCar.rotation.y)) * arrowFoward
+    xVelocity -= (acceleration * Math.sin(myCar.rotation.y)) * arrowBack
+
+    myCar.position.z += zVelocity
+    myCar.position.x += xVelocity
+
+    myCar.rotation.y += turnSpeed * arrowRight
+    myCar.rotation.y -= turnSpeed * arrowLeft
    
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
+    
+    zVelocity *= friction
+    xVelocity *= friction
 }
-animate()
